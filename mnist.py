b08ccbdfbe9a14b378e37c05b3a856bcdf7ed349
Arturo Carlos Rodriguez Cobo <art_rg@hotmail.com>
2019-12-03 23:02:54 +0100

    page 82

--- mnist.py
+++ mnist.py
@@ -13,7 +13,7 @@ X_test, y_test = mnist_test.loc[:,mnist_test.columns != "label"].values , mnist_
 print(X_test.shape)
 print(y_test.shape)
 
-# some_digit = X_train.iloc[36000]
+some_digit = X_train[36000]
 # some_digit_image = some_digit.values.reshape(28,28)
 # plt.imshow(some_digit_image, cmap = matplotlib.cm.binary, interpolation="nearest")
 # plt.axis("off")
@@ -22,3 +22,13 @@ print(y_test.shape)
 #%%
 shuffle_index = np.random.permutation(60000)
 X_train, y_train = X_train[shuffle_index] , y_train[shuffle_index]
+#%%
+y_train_5 = (y_train == 9)
+y_test_5 = (y_test == 9)
+#%%
+from sklearn.linear_model import SGDClassifier
+sgd_clf = SGDClassifier(random_state=42)
+sgd_clf.fit(X_train,y_train_5)
+sgd_clf.predict([some_digit])
+
+# %%
